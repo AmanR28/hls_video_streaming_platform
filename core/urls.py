0eb311fb111f19fc769f05b525f36ba746792212
Aman Rastogi <arastogi2810@gmail.com>
2024-07-08 03:11:39 +0530

    view all videos page

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("upload", views.upload),
+    path("", views.index),
     path("status/<uuid:video_id>/", views.status, name="status"),
     path("play/<uuid:video_id>/", views.play, name="play"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,18 +4,6 @@ from .forms import VideoForm
 from .models import Video
 
 
-def upload(request):
-    if request.method == "POST":
-        form = VideoForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect("status", video_id=form.instance.id)
-        else:
-            return render(request, "core/upload.html", {"form": form})
-
-    return render(request, "core/upload.html", {"form": VideoForm()})
-
-
 def status(request, video_id):
     video = Video.objects.get(id=video_id)
     return render(request, "core/status.html", {"video": video})
@@ -25,3 +13,26 @@ def play(request, video_id):
     video = Video.objects.get(id=video_id)
     url = video.play.url.split("?")[0]
     return render(request, "core/play.html", {"video": video, "url": url})
+
+
+def index(request):
+    if request.method == "POST":
+        form = VideoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("status", video_id=form.instance.id)
+        else:
+            return render(
+                request,
+                "core/index.html",
+                {"videos": Video.objects.order_by("-created_at").all(), "form": form},
+            )
+
+    return render(
+        request,
+        "core/index.html",
+        {
+            "videos": Video.objects.order_by("-created_at").all()[:8],
+            "form": VideoForm(),
+        },
+    )
